c01cd655c85b29607fc0d6ddc2f8ce0872747470
Ana Flávia <104928226+aflavinhams@users.noreply.github.com>
2023-05-17 22:23:21 -0300

    Add files via upload

--- jogoDaForca.py
+++ jogoDaForca.py
@@ -2,29 +2,29 @@
 # O jogo funciona apenas se você digitar as letras na ordem certa :/
 
 import random
-#Vetor com as palavras que serão randomizadas
+# Vetor com as palavras que serão randomizadas
 palavras = ["camelo", "cavalo", "ovelha", "coelho", "girafa"]
 
-#Randomização da palavra secreta
+# Randomização da palavra secreta
 indice = random.randint(0, 4)
 palavra = palavras[indice]
 print(palavra)
 
-#Criação dos vetores com as letras das palavras
+# Criação dos vetores com as letras das palavras
 camelo = ['c', 'a', 'm', 'e', 'l', 'o']
 cavalo = ['c', 'a', 'v', 'a', 'l', 'o']
 ovelha = ['o', 'v', 'e', 'l', 'h', 'a']
 coelho = ['c', 'o', 'e', 'l', 'h', 'o']
 girafa = ['g', 'i', 'r', 'a', 'f', 'a']
 
-#Criação dos vetores da palavra secreta e da resposta
+# Criação dos vetores da palavra secreta e da resposta
 vetorPalavra = [0] * 6
 vetorResposta = ['_'] * 6
 
-#Inicializando a variável tentativas em 8
+# Inicializando a variável tentativas em 8
 tentativas = 8
 
-#Bloco while para que a pessoa continue jogando enquanto não esgotar as tentativas
+# Bloco while para que a pessoa continue jogando enquanto não esgotar as tentativas
 while tentativas > 0:
     #print(f'Tentativas restantes: {tentativas}')
     print(f'Palavra: {vetorResposta}')
@@ -41,7 +41,7 @@ while tentativas > 0:
     elif palavra == 'girafa':
         vetorPalavra = girafa
 
-    #Bloco for para identificar a letra escolhida pelo usuário
+    # Bloco for para identificar a letra escolhida pelo usuário
     for i in range(len(vetorPalavra)):
 
         print(f'Tentativas restantes: {tentativas}')
@@ -54,10 +54,9 @@ while tentativas > 0:
         print(vetorResposta)
 
         if vetorResposta == vetorPalavra:
+            print('Parabéns! Você acertou a palavra secreta. ')
             break
-            # print('Parabéns! Você acertou a palavra secreta. ')
-            # break
-    print('Parabéns! Você acertou a palavra secreta. ')
+    # print('Parabéns! Você acertou a palavra secreta. ')
 
 if tentativas == 0:
     print('Suas tentativas acabaram! :(')
